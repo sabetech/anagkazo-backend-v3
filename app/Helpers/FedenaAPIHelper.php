c6033fd4605a16869028231782cc127fac40c4e9
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2023-06-28 07:39:54 +0000

    Decode xml response from fedena

--- app/Helpers/FedenaAPIHelper.php
+++ app/Helpers/FedenaAPIHelper.php
@@ -12,9 +12,14 @@ class FedenaAPIHelper {
             'Authorization' => 'Token token="'. env('FEDENA_API_TOKEN') .'"'
         ];
 
-        $response = Http::withHeaders($headers)->get(env('FEDENA_BASE_URL') . $indexNumber);
+        $response = Http::withHeaders($headers)->timeout(15)->get(env('FEDENA_BASE_URL') . $indexNumber);
         
         Log::info($response->body());
+        $xml = simplexml_load_string($body);
+        $jsonString = json_encode($xml);
+        $studentInfo = json_decode($jsonString, FALSE);
+
+        Log::info($studentInfo);
 
         /*
         $request = new Request('GET', env('FEDENA_BASE_URL') . $indexNumber, $headers);
