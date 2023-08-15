843bcdb6ebf17f4f22787c3ae84db3dbbb66c6ce
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2023-08-15 11:26:03 +0000

    added the cloudinary class to the app config

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
+	CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
