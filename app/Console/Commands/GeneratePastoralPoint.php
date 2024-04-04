29ae219ac0e742da957f116fce915cf2d1425053
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-04-04 12:56:24 +0000

    Fix command

--- app/Console/Commands/GeneratePastoralPoint.php
+++ app/Console/Commands/GeneratePastoralPoint.php
@@ -6,39 +6,28 @@ use Illuminate\Console\Command;
 use App\Models\Parameter;
 use App\Models\Student;
 
-class GeneratePastoralPoints extends Command
+class GeneratePastoralPoint extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'students:pastoral_points {class_id}';
+    protected $signature = 'app:generate-pastoral-point {class_id}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'This Command generates pastoral points for the class id specified.';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    protected $description = 'Command description';
 
     /**
      * Execute the console command.
-     *
-     * @return mixed
      */
     public function handle()
     {
+        //
         //
         $class_id = $this->argument('class_id');
         $handle = fopen("pastoral_point_{$class_id}.csv", 'w');
