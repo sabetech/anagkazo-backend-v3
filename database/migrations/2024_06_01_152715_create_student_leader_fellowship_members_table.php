e564eebdae60672661a4b08cd8d0eaa543eb0fbb
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-06-01 15:38:07 +0000

    student leader fellowship migration

---
+++ database/migrations/2024_06_01_152715_create_student_leader_fellowship_members_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_leader_fellowship', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("bacenta_leader_student_id")->constrained("students");
+            $table->foreignId("fellowship_leader_student_id")->constrained("students");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_leader_fellowship');
+    }
+};
